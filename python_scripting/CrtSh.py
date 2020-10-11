076c93fe6f05a29eb23f511936ef0fce080c3c53
Tore Malmedal <torem18@uia.no>
2020-10-11 05:06:17 +0200

    Added docstrings to methods

--- python_scripting/CrtSh.py
+++ python_scripting/CrtSh.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-#TODO: Make sure that PEP8 (style), PEP484 (type hints)
-# as well as PEP257 (docstrings) is followed.
+#TODO: PEP8
+#TODO: PEP257
+#TODO: PEP484
 
 import requests
 import argparse
@@ -13,30 +14,20 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 def argument_setup():
-    """
-    argument_setup [Set-up for command-line interface]
+    """Parses arguments from CLI
 
     Returns: 
-        namespace object
+        namespace object : Object containing the names and values of arguments.
     """
     parser = argparse.ArgumentParser(description="crt_sh subdomain searcher")
     parser.add_argument('--domain', metavar='domain', 
     help='the domain to check for subdomains (default: betauia.net)',
     default="betauia.net")
-    return parser.parse_args()  
+    return parser.parse_args()
 
 class CrtSh:
     def __init__(self, url: str):
-        """
-        Constructor for CrtSh.
-
-        Parameters:
-
-        Args:
-            url (str): [The url of to check for subdomains]
-
-        Returns:
-
+        """Constructor for CrtSh.
         """
 
         self.url = url
@@ -45,7 +36,7 @@ class CrtSh:
         self.date = date.today().strftime("%d-%m-%Y")
 
     def check_crtsh(self):
-        """Checks if https://crt.sh/ responds with 200
+        """Checks if crt.sh responds with 200
 
         Returns:
             r: Response object
@@ -60,7 +51,7 @@ class CrtSh:
 
     def validate_url_string(self, url:str):
         """Checks if the string seems to be a valid url using regex
-        
+
         Returns: True if match, false if not. 
         """
 
@@ -72,10 +63,10 @@ class CrtSh:
         return False
     
     def get_domains(self):
-        """
-        query_domain []
+        """Finds subdomains!
+
         Returns:
-        set of str
+            set: Set of subdomains. Single entry 'NaN' if no results are found.
         """
 
         payload = {'q': self.url}
@@ -86,10 +77,10 @@ class CrtSh:
 
         domains_set = set()
   
-        # The web page has 3 tables, whithout any ID, nested inside each other. 
-        # We want the third table
+        # The web page has 3 nested tables, whithout any ID. We want the 3rd.
         # Table headers: 
-        # [crt.sh ID, Logged at, Not Befor, Not After, Common Name, Matchin identities, Issuer Name]
+        # [crt.sh ID, Logged at, Not Before, Not After,
+        # Common Name, Matching identities, Issuer Name]
 
         if(len(tables) < 3):
             domains_set.add('NaN')
@@ -112,14 +103,31 @@ class CrtSh:
         return domains_set
     
     def valid_subdomains(self, domains_set: set):
+        """Validates a set of domains.
+
+        Args:
+            domains_set (set): A set of domains to be validated.
+
+        Returns:
+            set: A set of valid domains. Empty set if none are found.
+        """
+
         valid_domains = set()
         for domain in domains_set:
             if self.validate_url_string(domain):
                 valid_domains.add(domain)
         return valid_domains
 
-
     def check_subdomains(self, valid_set: set):
+        """Checks a set of valid domains for http response 200.
+
+        Checks a set of valid domains for http response 200. The results
+        are stored in class variables live_domains and dead_domains.
+
+        Args:
+            valid_set (set): A set of valid domains.
+        """
+
         for domain in valid_set:
             try:
                 r = requests.get(f'http://{domain}', timeout=2)
@@ -128,11 +136,14 @@ class CrtSh:
             except:
                 self.dead_domains.append(domain)
 
-    #TODO: Visit each active domain and grep header and store it together
-    # with the domain in a JSON file
-    # Vi vil ha <title>
-    
     def grep_title(self):
+        """Finds titles of live domains.
+
+        Finds titles of live domains.. Makes a GET request to each domain in
+        self.live_domains and tries to find the title. The tiles are saved to
+        a local JSON-file, using the super-domain name and date as title.
+        """
+
         titles = {}
         for domain in self.live_domains:
             r = requests.get(f'http://{domain}', timeout=3)
@@ -142,11 +153,13 @@ class CrtSh:
                 title = 'NaN'
             titles[domain] = title
 
-        with open(f'titles_{self.date}.json', "a") as write_file:
+        with open(f'{self.url}-{self.date}.json', "a") as write_file:
             json.dump(titles, write_file)
 
     def print_titles(self):
-        with open(f'titles_{self.date}.json') as read_file:
+        """Prints titles from local JSON-file
+        """
+        with open(f'{self.url}-{self.date}.json') as read_file:
             parsed = json.load(read_file)
         print(json.dumps(parsed, indent=2, sort_keys=True))
                 
